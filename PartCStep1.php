829078eb041b0386e8b24abe0a683a988acd50bc
Gr3gMcL3an <w0271296@nscc.ca>
2014-09-26 14:18:39 +0000

    Finished Part C Step 1 finished

--- PartCStep1.php
+++ PartCStep1.php
@@ -1,5 +1,17 @@
 <body>
 
+<?php
+$fileTmpName = $_FILES['userImage']['tmp_name'];
+$fileOrigName = $_FILES['userImage']['name'];
+$fileSize = $_FILES['userImage']['size'];
+$fileUploadError = $_FILES['userImage']['error']; // 0 means success
+$result = move_uploaded_file($fileTmpName,"uploads/".$fileOrigName);
+?>
+
+
+
+
+
 
     <?php
 
@@ -12,5 +24,30 @@
     Your last name is <?php echo $_POST['LastName']?>
     <p/>
     Your Height in meters is <?php echo $meters?>
+    <p/>
+    Tmp: <?php echo $fileTmpName?>
+    <p/>
+    Orig: <?php echo $fileOrigName ?>
+    <p/>
+    Size: <?php echo $fileSize?>
+    <p/>
+    Status:
+
+    <?php move_uploaded_file($fileTmpName,"pics/".$fileOrigName);?>
+
+    <?php
+
+    if($result == 0)
+    {
+        echo "Upload successful";
+    }else
+    {
+        "Upload not successful";
+    }
+
+
+
+    ?>
+
 
 </body>
